f953b5db2319b855c06367eef1dc9c57666b1f6a
fantik <fantik@zeoalliance.com>
2016-01-06 15:42:56 +0200

    add cache

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once __DIR__.'/vendor/autoload.php';
 
 $cleaner = new Prepare;
@@ -14,22 +13,31 @@ $source = file_get_contents('source.php');
 $tokens = token_get_all($source);
 $properties = $reflection->getProperties();
 
+$cache = new Cache();
+$cache->startRedis();
 
 foreach ($tokens as $token) {
-    if ($token[0]==373) {
-        $field['label'] = $labelClass->getLabel($token[1]);
-        $field['input'] = $cleaner->prepareToken($token[1]);
-        $field['input'] = $stringClass->getStringType($field['input']);
-        $field['input'] = $intClass->getIntType($field['input']);
-        $field['input'] = $lengthClass->getLength($field['input']);
-        $field['input'] = $rangeClass->getRange($field['input']);
-        $field['name']  = each($properties)['value']->name;
-        if ($field['label']) {
-            echo $field['label'];
-        } else {
-            echo $field['name'];
+
+    if ($token[0] == 373) {
+        $cachedAnnotation = $cache->getFromCache($token[1]);
+        if (!preg_match('/(param)/', $token[1]) && !preg_match('/(return)/', $token[1]) && !$cachedAnnotation) {
+            $field['label'] = $labelClass->getLabel($token[1]);
+            $field['name'] = each($properties)['value']->name;
+            $field['label'] ? null : $field['label'] = $field['name'];
+            $field['input'] = $cleaner->prepareToken($token[1]);
+            $field['input'] = $stringClass->getStringType($field['input']);
+            $field['input'] = $intClass->getIntType($field['input']);
+            $field['input'] = $lengthClass->getLength($field['input']);
+            $field['input'] = $rangeClass->getRange($field['input']);
+            $cachedField = $field['label'] . ': <input name ="' . $field['name'] . '"' . $field['input'] . ' />';
+            $cache->saveToCache($token[1], $cachedField);
+            echo $cachedField;
+            echo '<br>';
+        } elseif ($cachedAnnotation) {
+            echo $cachedAnnotation;
+            echo '<br>';
+
         }
-        echo ': <input name ="' . $field['name'] .'"' . $field['input'] . ' />';
-        echo '<br>';
     }
 }
+$cache->closeRedis();
